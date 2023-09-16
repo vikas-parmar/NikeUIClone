f97cef90c0d2d92d603ac7f849027b8cce08a6b1
Vikas Parmar <vkparmar003@gmail.com>
2023-09-16 22:04:52 +0530

    Made Three Functional components. Added Hero components showing home section. Made Navbar with hamburger menu button. Added a separate button component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,13 @@
-const App = () => <h1>Vikas Parmar</h1>;
+import Nav from "./components/Nav";
+import { Hero } from "./sections";
+
+const App = () => (
+  <main className="relative">
+    <Nav />
+    <section className="xl:padding-l wide:padding-r padding-b">
+      <Hero />
+    </section>
+  </main>
+);
 
 export default App;
